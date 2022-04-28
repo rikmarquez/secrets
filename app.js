5eb322f8df66c6de6e9af3b0b3f3658f6fdd06f1
Rik Marquez <rik@rikmarquez.com>
2022-04-28 17:44:31 -0500

    Environment vars for encrypting

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -26,8 +27,7 @@ const userSchema = new mongoose.Schema({
 });
 
 // Setup Encriptacion en mongoose-encryption
-const secret = "estesmylittlesecretqueseguronosabes";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
